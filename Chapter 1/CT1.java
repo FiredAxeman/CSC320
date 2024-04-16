db12b8cab6517215616c415ff70ec6447acde43f
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-04-15 18:20:11 -0600

    added CT1

---
+++ Chapter 1/CT1.java
@@ -0,0 +1,11 @@
+public class CT1 {
+    public static void main(String[] args) {
+        System.out.println("Harley");
+        System.out.println("Driscoll");
+        System.out.println("100 Main Street");
+        System.out.println("Pueblo");
+        System.out.println("81089");
+
+     }
+    
+}

92e1576ab16652d821e3928741df78d15ebd4f3f
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-04-18 15:55:01 -0600

    updates added chapter 2

---
+++ Chapter2/Coins.java
@@ -0,0 +1,19 @@
+package Chapter2;
+
+public class Coins {
+    public static void main(String[] args) {
+        int numCoins;
+        int numNickels;
+        int numDimes;
+  
+        numNickels = 5;
+        numDimes = 6;
+  
+        /* Your solution goes here  */
+        numCoins = numNickels + numDimes;
+  
+        System.out.print("There are ");
+        System.out.print(numCoins);
+        System.out.println(" coins");
+     }
+}

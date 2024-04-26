2f3f1c9fec0bfeec31e0f9df598ccd2896fbc993
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-04-26 11:53:41 -0500

    Added chapter 3

---
+++ Chapter3/ConvertPositive.java
@@ -0,0 +1,23 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class ConvertPositive {
+    public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userNum;
+
+      userNum = scnr.nextInt();
+
+      /* Your solution goes here  */
+      if (userNum > 0) {
+         System.out.println("Positive.");
+      }
+      else {
+         System.out.println("Non-positive, converting to 1.");
+         userNum = 1;
+      }
+      System.out.println("Final: " + userNum);
+
+   }
+}

d4b5a5f5987b3b7dc77252e581a40867141677c8
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-04-29 16:19:29 -0500

    Added Discussion 3

---
+++ Chapter3/Discussion3.java
@@ -0,0 +1,24 @@
+package Chapter3;
+import System.java.util.Scanner;
+
+public class Discussion3 {
+    public static void main(String[] args) {
+        java.util.Scanner scnr = new java.util.Scanner(System.in);
+        int userNum;
+        System.out.println("Enter a number: ");
+        userNum = scnr.nextInt();
+        if (userNum < 0) {
+            System.out.println("Your number " + userNum + " is Negative");
+        }
+        else if (userNum == 1){
+            System.out.println("Number is one.");
+        }
+        else if (userNum >= 100){
+            System.out.println("Your Number " + userNum + " is greater than or equal to 100.");
+        }
+        else {
+            System.out.println("Your number " + userNum + " is Positive.");
+        }
+        scnr.close();
+    }
+}

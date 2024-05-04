ddc0035138c26605d1f1294e7b7690a368b105ad
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-05-04 15:01:17 -0500

    removed fixme's

--- Labs/BasicInput.java
+++ Labs/BasicInput.java
@@ -9,7 +9,7 @@ public class BasicInput {
       double userDouble;
       char userChar;
       String userString;
-      // FIXME Define char and string variables similarly
+     
       
       System.out.println("Enter integer:");
       userInt = scnr.nextInt();
@@ -27,10 +27,6 @@ public class BasicInput {
       
       
       
-      // FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-   
-      // FIXME (2): Output the four values in reverse
       
-      // FIXME (3): Cast the double to an integer, and output that integer
    }
 }
--- Labs/PaintEstimator.java
+++ Labs/PaintEstimator.java
@@ -19,11 +19,6 @@ public class PaintEstimator {
       System.out.println("Paint needed: " + (wallArea / paint) + " gallons");
       System.out.println("Cans needed: " + (int)Math.round(wallArea/paint) + " can(s)");
       scnr.close();
-      // FIXME (1): Prompt user to input wall's width
-      // Calculate and output wall area
-      // FIXME (1): Calculate the wall's area
-      // FIXME (1): Finish the output statement
-      // FIXME (2): Calculate and output the amount of paint in gallons needed to paint the wall
-      // FIXME (3): Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+      
    }
 }

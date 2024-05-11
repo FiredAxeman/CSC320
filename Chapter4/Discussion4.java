7f8206c964e18e0634fbe7e22112833b344f8a64
Mark Watney <79179990+FiredAxeman@users.noreply.github.com>
2024-05-11 10:43:21 -0500

    updates chapter 4

--- Chapter4/Discussion4.java
+++ Chapter4/Discussion4.java
@@ -2,20 +2,21 @@ package Chapter4;
 import java.util.Scanner;
 public class Discussion4 {
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        int userChoice;
-        int i;
-        int userNumber;
-        int total;
-        total = 0;
-        System.out.println("Please enter the amount of numbers you wish to add: ");
-        userChoice = scnr.nextInt();
-        for (i = 0; i < userChoice; i++) {
-        System.out.println("Please enter a number: ");
-        userNumber = scnr.nextInt();
-        total = total + userNumber;
-       }
-         System.out.println("The total of the numbers entered is: " + total);
+            Scanner scnr = new Scanner(System.in);
+            int userChoice;
+            int i;
+            int userNumber;
+            int total;
+            total = 0;
+            System.out.println("Please enter the amount of numbers you wish to add: ");
+            userChoice = scnr.nextInt();
+            for (i = 0; i < userChoice; i++) {
+            System.out.println("Please enter a number: ");
+            userNumber = scnr.nextInt();
+            total = total + userNumber;
+         }
+             
+             System.out.println("The total of the numbers entered is: " + total);
          scnr.close();
     }
 }
